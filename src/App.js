6b7f9d5e930fb5c62ab86643382c93871b74c0be
MHossain <mhh52728@gmail.com>
2022-12-18 18:25:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavScrollExample from './Components/Nav/Nav';
+import Body from './Components/Body/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <NavScrollExample></NavScrollExample>
+      <Body></Body>
+    </div>
+  );
+}
+
+export default App;
